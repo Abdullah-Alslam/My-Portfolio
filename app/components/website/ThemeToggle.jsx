7cdfc2cee2583aa20b3e17cb38117db9217fd8ec
Abdullah <abdullahxyzabc67@gmail.com>
2025-10-06 20:42:14 +0300

    feat : create Dark mode

--- app/components/website/ThemeToggle.jsx
+++ app/components/website/ThemeToggle.jsx
@@ -1,23 +1,22 @@
-// components/ThemeToggle.jsx
 "use client";
+
 import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
-export default function ThemeToggle() {
+export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  // نتأكد أن الكود يشتغل بعد التحميل على المتصفح فقط
   useEffect(() => setMounted(true), []);
-
-  if (!mounted) return null; // يمنع Hydration Mismatch
+  if (!mounted) return null;
 
   return (
     <button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="bg-white text-black dark:bg-black dark:text-white px-3 py-1 rounded transition"
+      className="p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 transition"
     >
-      {theme === "light" ? "🌙" : "☀️"}
+      {theme === "light" ? <Moon /> : <Sun />}
     </button>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class", // ✅ هذا السطر مهم جداً
+
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
