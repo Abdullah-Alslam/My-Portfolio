637b487edf1c52eae8bac63fd84e4cc1e0316ba9
Abdullah <abdullahxyzabc67@gmail.com>
2025-10-07 11:46:37 +0300

    feat(Home) : add    <HeroSection />       <AboutSection />       <ProjectsSection />       <SkillsSection />       <ContactCTA/> to Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div className="bg-red ">home page</div>;
+import AboutSection from "./components/Home/AboutSection";
+import ContactCTA from "./components/Home/ContactCTA";
+import HeroSection from "./components/Home/HeroSection";
+import ProjectsSection from "./components/Home/ProjectsSection";
+import SkillsSection from "./components/Home/SkillsSection";
+
+
+export default function HomePage() {
+  return (
+    <div className="pt-16">
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <ContactCTA/>
+    </div>
+  );
 }
