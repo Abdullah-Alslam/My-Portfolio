a2da4bdcf534b41b9bc8ffe4f6c5db75f3fdc95b
Abdullah <abdullahxyzabc67@gmail.com>
2025-10-07 11:41:11 +0300

    feat : create dark mode

---
+++ app/components/Dark mode/ThemeToggle.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className="p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 transition"
+    >
+      {theme === "light" ? <Moon /> : <Sun />}
+    </button>
+  );
+}
