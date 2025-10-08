e79b79de2772c8947138467b0043434a5716894e
Abdullah <abdullahxyzabc67@gmail.com>
2025-10-08 14:02:50 +0300

    chore(home) :remove     <SkillsSection />       <ContactCTA/> from homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,6 @@
 import AboutSection from "./components/Home/AboutSection";
-import ContactCTA from "./components/Home/ContactCTA";
 import HeroSection from "./components/Home/HeroSection";
 import ProjectsSection from "./components/Home/ProjectsSection";
-import SkillsSection from "./components/Home/SkillsSection";
 
 
 export default function HomePage() {
@@ -11,8 +9,6 @@ export default function HomePage() {
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
-      <SkillsSection />
-      <ContactCTA/>
     </div>
   );
 }
