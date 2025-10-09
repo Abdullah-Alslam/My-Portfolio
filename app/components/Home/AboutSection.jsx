8653f0cae42b571c2849016b71b3196ece9d347f
Abdullah <abdullahxyzabc67@gmail.com>
2025-10-09 14:53:34 +0300

    feat(Home) : add text to aboutsection

--- app/components/Home/AboutSection.jsx
+++ app/components/Home/AboutSection.jsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 export default function AboutSection() {
   return (
     <section className="relative py-24 bg-gray-50 dark:bg-gray-900 flex flex-col items-center justify-center px-6 md:px-16">
-      
       {/*about*/}
       <motion.div
         initial={{ opacity: 0, y: 30 }}
@@ -18,18 +17,42 @@ export default function AboutSection() {
           About Me
         </h2>
         <p className="text-lg md:text-xl text-gray-800 dark:text-gray-300 mb-6 leading-relaxed">
-          Hello! I’m <span className="font-semibold text-blue-600 dark:text-blue-400">Abdullah Abdalsalam</span>, 
-          a passionate Frontend Developer who loves building <span className="font-semibold text-black dark:text-white">interactive</span>, 
-          <span className="font-semibold text-blue-500 dark:text-blue-300">responsive</span>, and visually appealing web experiences. 
-          I work mainly with <span className="font-semibold">Next.js</span>, <span className="font-semibold">React</span>, 
-          <span className="font-semibold">TypeScript</span>, and modern web technologies.
+          Hello! I’m{" "}
+          <span className="font-semibold text-blue-600 dark:text-blue-400">
+            Abdullah Abdalsalam
+          </span>
+          , a passionate Frontend Developer who loves building{" "}
+          <span className="font-semibold text-black dark:text-white">
+            interactive
+          </span>
+          ,
+          <span className="font-semibold text-blue-500 dark:text-blue-300">
+            responsive
+          </span>
+          , and visually appealing web experiences. I work mainly with{" "}
+          <span className="font-semibold">Next.js</span>,{" "}
+          <span className="font-semibold">React</span>,
+          <span className="font-semibold">TypeScript</span>, and modern web
+          technologies.
         </p>
 
         {/* tags*/}
         <div className="flex flex-wrap gap-3 justify-center mt-4">
           {[
-            "React", "Next.js", "TypeScript", "Tailwind CSS", "Firebase",
-            "Redux", "Vite", "Node.js", "Framer Motion", "Responsive Design"
+            "React",
+            "Next.js",
+            "javascript",
+            "Tailwind CSS",
+            "Firebase",
+            "Bootstrap",
+            "Vite",
+            "Node.js",
+            "Framer Motion",
+            "Responsive Design",
+            "HTML",
+            "CSS",
+            "GIT & GITHUB",
+            
           ].map((tech) => (
             <span
               key={tech}
