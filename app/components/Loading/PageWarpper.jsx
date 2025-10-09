1063b27fd953b0963b4062e17416adc16b1250f1
Abdullah <abdullahxyzabc67@gmail.com>
2025-10-09 14:27:10 +0300

    feat : create PageLoading

---
+++ app/components/Loading/PageWarpper.jsx
@@ -0,0 +1,17 @@
+"use client"; 
+
+import { useState, useEffect } from "react";
+import PageLoading from "./PageLoading";
+
+export default function PageWrapper({ children }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 800); 
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <PageLoading color="#f87171" bg="bg-gray-900" />;
+
+  return <>{children}</>; 
+}
